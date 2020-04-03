75ca7d72a9469ff0d74a2c81b8680190f0853784
Ashok <ashokpatidar80@gmail.com>
2020-04-03 23:30:05 +0530

    ⚙️ enviroment setup ⚙️

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import * as dotEnv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from './app/app.module';
+import { Logger } from '@nestjs/common';
+
+// Configure environment file with project
+dotEnv.config();
+
+// Read port number from env file
+const port = process.env.PORT || 4001;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Create nestFactory instance for make server instance
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  Logger.log(`🚀  Server ready at http://localhost:${port} `, 'ServerStarted');
+  // Run server on port
+  await app.listen(port);
 }
 bootstrap();
